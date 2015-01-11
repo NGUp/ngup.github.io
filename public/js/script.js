fe14eca8fd4bca9e0338ad3bebcacfa867732e5a
namvh <vhnam2504@gmail.com>
2015-01-11 23:00:54 +0700

    fix Angular Routing not working.

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,20 @@
-angular.module("ngup", ["route"])
+var ngup = angular.module('ngup', ['ngRoute']);
+
+ngup.config(function($routeProvider) {
+    $routeProvider
+
+        .when('/', {
+            templateUrl : 'view/index.html',
+            controller  : 'mainController'
+        })
+
+        .when('/httt', {
+            templateUrl : 'view/httt.html',
+            controller  : 'aboutController'
+        })
+
+        .when('/110001np', {
+            templateUrl : 'view/110001np.html',
+            controller  : 'contactController'
+        });
+});
