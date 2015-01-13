f5c0d3cbeb89eedb6115b8d6c65dc5dfc06fbada
namvh <vhnam2504@gmail.com>
2015-01-13 15:03:23 +0700

    load HTTT's members

--- public/js/controller/team110001NP.js
+++ public/js/controller/team110001NP.js
@@ -1,7 +1,16 @@
-package.controller.team110001NP = function($scope, $location) {
+package.controller.team110001NP = function($scope) {
 	$.getJSON('/data/ngup.json', function(data) {
-		$scope.people = data.team110001NP;
+		// Initialize package.team.team110001NP
+		package.team.team110001NP = data.team110001NP;
+		package.team.teamHTTT = data.teamHTTT;
+
+		// For first page load
+		$scope.team110001NP = package.team.team110001NP;
+		$scope.teamHTTT = package.team.teamHTTT;
     }).error(function(data) {
     	console.log(data);
     });
+
+    // Initialize for ngRepeat
+    $scope.team110001NP = package.team.team110001NP;
 }
--- public/js/controller/teamHTTT.js
+++ public/js/controller/teamHTTT.js
@@ -1,3 +1,16 @@
 package.controller.teamHTTT = function($scope) {
-	console.log("HTTT");
+	$.getJSON('/data/ngup.json', function(data) {
+		// Initialize package.team.team110001NP
+		package.team.teamHTTT = data.teamHTTT;
+		package.team.team110001NP = data.team110001NP;
+
+		// For first page load
+		$scope.teamHTTT = package.team.teamHTTT;
+		$scope.team110001NP = package.team.team110001NP;
+    }).error(function(data) {
+    	console.log(data);
+    });
+
+    // Initialize for ngRepeat
+    $scope.teamHTTT = package.team.teamHTTT;
 }
